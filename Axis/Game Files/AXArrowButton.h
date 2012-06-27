ca4b694e14e10196f7d401ac38ff5e6c8a046faa
Jethro Wilson <Jet@Jethros-MacBook.local>
2012-06-27 21:50:49 +0100

    Enhanced Button

---
+++ Axis/Game Files/AXArrowButton.h
@@ -0,0 +1,13 @@
+//
+//  AXArrowButton.h
+//  Axis
+//
+//  Created by Jethro Wilson on 27/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AXButton.h"
+
+@interface AXArrowButton : AXButton
+
+@end
---
+++ Axis/Game Files/AXArrowButton.m
@@ -0,0 +1,32 @@
+//
+//  AXArrowButton.m
+//  Axis
+//
+//  Created by Jethro Wilson on 27/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AXArrowButton.h"
+
+#pragma mark arrow button mesh
+static NSInteger BBArrowButtonOutlineVertexesCount = 14;
+static CGFloat BBArrowButtonOutlineVertexes[28] = {-0.25, 0.0, 0.25, 0.0,
+    0.25, 0.0, 0.1, 0.25,0.25, 0.0, 0.1,-0.25, -0.5,-0.5,-0.5, 0.5, -0.5, 0.5, 
+    0.5, 0.5, 0.5, 0.5, 0.5,-0.5, 0.5,-0.5,-0.5,-0.5};
+static GLenum BBArrowButtonOutlineRenderStyle = GL_LINES;
+static CGFloat BBArrowButtonOutlineColorValues[56] = 
+{1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0,
+    1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0,
+    1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0,
+    1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0};
+
+@implementation AXArrowButton
+
+- (void)setNotPressedVertexes {
+    mesh.vertexes = BBArrowButtonOutlineVertexes;
+    mesh.vertexCount = BBArrowButtonOutlineVertexesCount;
+    mesh.renderStyle = BBArrowButtonOutlineRenderStyle;
+    mesh.colors = BBArrowButtonOutlineColorValues;
+}
+
+@end
