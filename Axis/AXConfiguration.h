7fbd4940450ab829ec6b74f296a06e9e0cc902bc
Jethro Wilson <Jet@Jethros-MacBook.local>
2012-07-11 10:43:40 +0100

    Axis Configuration Settings

--- Axis/AXConfiguration.h
+++ Axis/AXConfiguration.h
@@ -6,18 +6,31 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+// Engine Setup
+#define AX_USE_DISPLAY_LINK 1
+#define AX_USE_POINT_PER_SECOND 0
+#define AX_USE_CONVERT_IMAGE_LOW_BITMAP 0
+#define AX_ENABLE_RETINA_DISPLAY 1
+
+// Engine Debug
+#define AX_DEBUG_DRAW_COLLIDERS 1
+
+// Engine Console Debug
+#define AX_CONSOLE_DISPLAY_ALL_FRAME_RATES 0
+#define AX_CONSOLE_LOW_FRAME_RATE_WARNING 0
+#define AX_CONSOLE_LOW_FRAME_RATE_WARNING_MARK 40.0
+
+// Engine Calculus
 #define RANDOM_SEED() srandom(time(NULL))
 #define RANDOM_INT(__MIN__, __MAX__) ((__MIN__) + random() % ((__MAX__+1) - (__MIN__)))
 
-// will draw the circles around the collision radius
-// for debugging
-#define DEBUG_DRAW_COLLIDERS 1
+// a handy constant to keep around
+#define AX_CALC_RADIANS_TO_DEGREES 57.2958
+
+// Game Constants
 
 // the explosive force applied to the smaller rocks after a big rock has been smashed
 #define SMASH_SPEED_FACTOR 0.75
 
 #define TURN_SPEED_FACTOR 3.0
 #define THRUST_SPEED_FACTOR 0.02
-
-// a handy constant to keep around
-#define BBRADIANS_TO_DEGREES 57.2958
