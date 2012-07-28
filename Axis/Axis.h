2f2442db9ba943d859d7637c110f1d480b55b547
Jethro Wilson <Jet@Jethros-MacBook.local>
2012-07-28 19:22:14 +0100

    Added Completed and Todo

--- Axis/Axis.h
+++ Axis/Axis.h
@@ -21,6 +21,9 @@
 #define AXIS_VERSION_FORM @"Base"
 #define AXIS_VERSION_TYPE @"Alpha"
 
+#define AXIS_JUST_COMPLETED @"Parent/Child full support | Started adding AXObject for base child support, May not support."
+#define AXIS_TODO @"Major code cleanup | Multi-scene update support"
+
 // Support - If set to off, components are not loaded
 #define AXIS_SUPPORT_THE_GAME_ENGINE 1 // The Game Engine. If set to off, Game Engine is not complete
 #define AXIS_SUPPORT_SOLAR_GAME_KIT 0 // Solar Game Kit for Game Center
